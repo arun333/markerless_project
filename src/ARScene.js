341b5909ca75143c9a04fdf8ecd40a8bd9f227db
unknown <arun1anim@gmail.com>
2025-08-28 20:06:50 +0930

    Animated Shark updated

--- src/ARScene.js
+++ src/ARScene.js
@@ -56,7 +56,7 @@ const ARScene = () => {
                 loader.load('models/shark.glb', (gltf) => {
                 dolphinModel = gltf.scene;
                 dolphinModel.position.setFromMatrixPosition(reticle.matrix);
-                dolphinModel.scale.set(0.5, 0.5, 0.5);
+                dolphinModel.scale.set(0.08, 0.08, 0.08);
                 scene.add(dolphinModel);
 
                 console.log("Model placed at", dolphinModel.position);
