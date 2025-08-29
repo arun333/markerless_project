cacdace04b103f0e4f89882d636b26ee21270c36
unknown <arun1anim@gmail.com>
2025-08-30 00:02:01 +0930

    Model switch

--- src/ARScene.js
+++ src/ARScene.js
@@ -179,6 +179,12 @@ const ARScene = () => {
              // dolphinModel.lookAt(cameraPosition); 
 
     }
+
+    if (model && selectedModel === 'whale.glb') {
+        const t = clock.getElapsedTime();
+        model.position.x = Math.sin(t) * 0.5;
+        model.position.z = Math.cos(t) * 0.5;
+    }
      const delta = clock.getDelta();
       if (mixer) mixer.update(delta);
 
